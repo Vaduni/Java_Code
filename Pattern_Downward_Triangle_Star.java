d229b0ce034cc73ba4d4d6395205aafcee440208
Vaduni Niranjan <vaduniniranjan@gmail.com>
2024-11-11 14:34:52 +0530

    Downward Triangle star Pattern

---
+++ Pattern_Downward_Triangle_Star.java
@@ -0,0 +1,16 @@
+package Java_Code;
+import java.util.Scanner;
+public class Pattern_Downward_Triangle_Star{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter number of rows : ");
+        int rows = sc.nextInt();
+        sc.close();
+        for(int i=rows-1;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+                System.out.println("");
+            }
+        }
+    }
