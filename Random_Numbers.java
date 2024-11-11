bca259fda9eb65a295a18da334b17e439645af1a
Vaduni Niranjan <vaduniniranjan@gmail.com>
2024-11-11 09:58:47 +0530

    Generate Random Numbers

--- Random_Numbers.java
+++ Random_Numbers.java
@@ -4,6 +4,6 @@ import java.lang.Math;
 
 public class Random_Numbers {
     public static void main(String[] args) {
-        System.out.println("random number is " + Math.random());
+        System.out.println("Random number is " + Math.random());
     }
 }
