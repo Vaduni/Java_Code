aa45810cd95ac6b003fb6b76f24f839343962514
Vaduni Niranjan <vaduniniranjan@gmail.com>
2024-11-11 09:47:28 +0530

    Generate Random No. within range

--- Random_Number_Range.java
+++ Random_Number_Range.java
@@ -4,9 +4,9 @@ import java.util.Random;
 
 public class Random_Number_Range {
     public static void main(String[] args) {
-        // to generate random number within upperbound
+        // to generate randome number within upperbound
         Random r = new Random();
-        int random = r.nextInt(1000);
+        int random = r.nextInt(10000);
         System.out.println(random);
     }
 }
