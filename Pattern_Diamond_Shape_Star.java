055e00beeb82095fc016c2d261a3c9737cb809cb
Vaduni Niranjan <vaduniniranjan@gmail.com>
2024-11-11 10:59:39 +0530

    Diamond Shape Pattern

---
+++ Pattern_Diamond_Shape_Star.java
@@ -0,0 +1,34 @@
+package Java_Code;
+
+import java.util.Scanner;
+
+public class Pattern_Diamond_Shape_Star {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter number of rows");
+        int rows = sc.nextInt();
+        sc.close();
+
+        // upper half of diamond
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // lower half of diamond
+        for (int i = rows - 1; i > 0; i--) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
