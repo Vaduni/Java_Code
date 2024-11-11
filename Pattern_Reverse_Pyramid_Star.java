b01784a55735504928ca743436530a7a18ab0151
Vaduni Niranjan <vaduniniranjan@gmail.com>
2024-11-11 19:12:20 +0530

    Reverse pyramid star pattern

--- Pattern_Reverse_Pyramid_Star.java
+++ Pattern_Reverse_Pyramid_Star.java
@@ -8,15 +8,15 @@ public class Pattern_Reverse_Pyramid_Star {
         System.out.println("enter number of rows");
         int rows = sc.nextInt();
         sc.close();
-        for(int i=rows;i>=1;i--){
-            for(int j=i;j<=rows;j++){
+        for (int i = rows; i >= 1; i--) {
+            for (int j = i; j <= rows; j++) {
                 System.out.print(" ");
             }
-            for(int j=1;j<=(2*i-1);j++){
-                System.out.print(" * ");
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
-        
+
     }
 }
