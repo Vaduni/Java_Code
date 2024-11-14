037982647e43ffa355e1b02ca0954f1657841dd4
Vaduni Niranjan <vaduniniranjan@gmail.com>
2024-11-14 10:22:00 +0530

    Triangle with 0,1

---
+++ Pattern_Triangle_01.java
@@ -0,0 +1,23 @@
+package Java_Code;
+
+import java.util.Scanner;
+
+public class Pattern_Triangle_01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of rows : ");
+        int rows = sc.nextInt();
+        sc.close();
+        for(int i=1;i<=rows;i++){
+            for(int j=1;j<=i;j++){
+                if((i+j)%2==0){
+                    System.out.print(1);
+                }
+                else{
+                    System.out.print(0);
+                }
+            }
+            System.out.println();
+        }
+    }
+}
