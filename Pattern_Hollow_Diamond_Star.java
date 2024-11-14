1e9798fbab320f8710a7cc4c9a3e5a33232b2777
Vaduni Niranjan <vaduniniranjan@gmail.com>
2024-11-14 17:32:50 +0530

    Hollow Diamond

---
+++ Pattern_Hollow_Diamond_Star.java
@@ -0,0 +1,46 @@
+package Java_Code;
+
+import java.util.Scanner;
+
+public class Pattern_Hollow_Diamond_Star {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter number of rows");
+        int rows = sc.nextInt();
+        sc.close();
+
+        // for upper half
+        for (int i = 1; i <= rows; i++) {
+            for (int j = rows; j > i; j--) {
+                System.out.print(" ");
+            }
+
+            // printing stars and spaces for hollow part
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                if (j == 1 || j == 2 * i - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        // for lower half
+        for (int i = rows - 1; i >= 1; i--) {
+            for (int j = rows; j > i; j--) {
+                System.out.print(" ");
+            }
+
+            // for printing stars and spaces for hollow part
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                if (j == 1 || j == 2 * i - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
