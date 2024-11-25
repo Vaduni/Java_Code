08dc3d486c652bca06589331329c8c862965c564
Vaduni Niranjan <vaduniniranjan@gmail.com>
2024-11-25 19:14:40 +0530

    Diamond pattern using Numbers

--- Pattern_Diamond_Numbers.java
+++ Pattern_Diamond_Numbers.java
@@ -9,35 +9,35 @@ public class Pattern_Diamond_Numbers {
         int rows = sc.nextInt();
         sc.close();
 
-        //upper half 
-        //outer loop for printing rows
-        for(int i=1;i<=rows;i++){
-            //inner loop for printing spaces
-            for(int j=i;j<rows;j++){
+        // upper half
+        // outer loop for printing rows
+        for (int i = 1; i <= rows; i++) {
+            // inner loop for printing spaces
+            for (int j = i; j < rows; j++) {
                 System.out.print(" ");
             }
-            //inner loop for printing descending numbers
-            for(int k=i;k>=1;k--){
-            System.out.print(k);
+            // inner loop for printing descending numbers
+            for (int k = i; k >= 1; k--) {
+                System.out.print(k);
+            }
+            // printing ascending numbers
+            for (int k = 2; k <= i; k++) {
+                System.out.print(k);
+            }
+            System.out.println();
         }
-        //printing ascending numbers
-        for(int k=2;k<=i;k++){
-            System.out.print(k);
+        // lower half
+        for (int i = rows - 1; i >= 1; i--) {
+            for (int j = rows; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = i; k >= 1; k--) {
+                System.out.print(k);
+            }
+            for (int k = 2; k <= i; k++) {
+                System.out.print(k);
+            }
+            System.out.println();
         }
-        System.out.println();
-    }
-    //lower half
-    for(int i=rows-1;i>=1;i--){
-        for(int j=rows;j>i;j--){
-            System.out.print(" ");
-        }
-        for(int k=i;k>=1;k--){
-            System.out.print(k);
-        }
-        for (int k=2;k<=i;k++){
-            System.out.print(k);
-        }
-        System.out.println();
-    }
     }
 }
