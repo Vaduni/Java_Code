0555464d0b530a7ddb235e10b566f797351f9aa5
Vaduni Niranjan <vaduniniranjan@gmail.com>
2024-12-07 12:03:45 +0530

    sum function

--- Array_Basic.java
+++ Array_Basic.java
@@ -2,18 +2,20 @@ package Java_Code;
 
 import java.util.Scanner;
 
+//Taking an array from user and searching for a number.
+
 public class Array_Basic {
     public static void main(String[] args) {
         int arr[] = new int[4];
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter numbers: ");
-        for(int i=0;i<arr.length;i++){
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == 4) {
                 System.out.println("Match found at index : " + i);
-            } 
+            }
         }
 
         sc.close();
