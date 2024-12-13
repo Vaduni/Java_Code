d10dc28ffc4cc7110ea32de1f8aa66d87cc8c43f
Vaduni Niranjan <vaduniniranjan@gmail.com>
2024-12-13 22:02:24 +0530

    Selection sort

---
+++ Sort_Selection.java
@@ -0,0 +1,28 @@
+package Java_Code;
+
+//time complexity - O(n^2)
+
+public class Sort_Selection {
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 7, 4, 8, 3 };
+        for (int i = 0; i < arr.length - 1; i++) {
+            int smallest = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[smallest]) {
+                    smallest = j;
+                }
+            }
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+        }
+        printArray(arr);
+    }
+}
