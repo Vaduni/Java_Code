87ad66a4f9c5862377ea95f1a7da7d03901df030
Vaduni Niranjan <vaduniniranjan@gmail.com>
2024-12-19 12:11:43 +0530

    Tower Of hanoi

--- Recursion_TowerOfHanoi.java
+++ Recursion_TowerOfHanoi.java
@@ -1,16 +1,20 @@
 package Java_Code;
 
-public class Recursion_ToweOfHanoi {
-    public static void Transfer(int n, String source, String helper,String dest){
-        //base case
-        if(n==1){
-            System.out.println("transferring disk " +n+ " from "+source+" to "+dest);
+//no of steps : (2^n -1)
+//time complexity : 2^n
+
+public class Recursion_TowerOfHanoi {
+    public static void Transfer(int n, String source, String helper, String dest) {
+        // base case
+        if (n == 1) {
+            System.out.println("transferring disk " + n + " from " + source + " to " + dest);
             return;
         }
-        Transfer(n-1, source, dest,helper);
-        System.out.println("Transfer disk " +n+ " from "+source+" to "+dest);
-        Transfer(n-1, helper, source, dest);
+        Transfer(n - 1, source, dest, helper);
+        System.out.println("Transfer disk " + n + " from " + source + " to " + dest);
+        Transfer(n - 1, helper, source, dest);
     }
+
     public static void main(String[] args) {
         Transfer(3, "S", "H", "D");
     }
