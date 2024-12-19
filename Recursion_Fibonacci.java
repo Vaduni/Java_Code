c495c4f51097c4c92b6a06a06e917ae5552dfb85
Vaduni Niranjan <vaduniniranjan@gmail.com>
2024-12-19 09:49:51 +0530

    Fibonacci using recursion

--- Recursion_Fibonacci.java
+++ Recursion_Fibonacci.java
@@ -1,12 +1,12 @@
 package Java_Code;
 
 public class Recursion_Fibonacci {
-    public static void printFibonacci(int n1,int n2,int n){
-        if(n==0){
-        return;
+    public static void printFibonacci(int n1, int n2, int n) {
+        if (n == 0) {
+            return;
         }
-        System.out.print(n1);
-printFibonacci(n2, n1+n2, n-1); //n-1 becuase already printed one term a
+        System.out.print(n1 + " ");
+        printFibonacci(n2, n1 + n2, n - 1); // n-1 becuase already printed one term a
     }
     public static void main(String[] args) {
         printFibonacci(0, 1, 7);
