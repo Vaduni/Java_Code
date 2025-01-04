f66267b05943bb88d6197dabdf5cd1ba217223b4
Vaduni Niranjan <vaduniniranjan@gmail.com>
2025-01-04 18:54:55 +0530

    Linked List basics

---
+++ LinkedList.java
@@ -0,0 +1,89 @@
+package Java_Code;
+
+public class LinkedList {
+    Node head;
+    class Node {
+        String data;
+        Node next;
+        
+        Node(String data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    //add first
+    public void addFirst(String data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=newNode;
+        }
+        else{
+            newNode.next=head;
+            head=newNode;
+        }
+    }
+    //add last
+    public void addLast(String data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=newNode;
+        }
+        else {
+            Node currNode=head;
+            while(currNode.next!=null){
+                currNode=currNode.next;
+            }
+            currNode.next=newNode;
+        }
+    }
+    //print list
+    public void printList(){
+        if(head==null){
+            System.out.println("List is empty");
+        }
+        Node currNode=head;
+        while(currNode!=null){
+            System.out.print(currNode.data+"->");
+            currNode=currNode.next;
+        }
+        System.out.println("null");
+    }
+    //delete first
+    public void deleteFirst(){
+        if(head==null){
+            System.out.println("List is empty");
+        }
+        else{
+            head=head.next;
+        }
+    }
+    //delete last
+    public void deleteLast(){
+        if(head==null){
+            System.out.println("List is empty");
+        }
+        else{
+            if(head.next==null){
+                head=null;
+            }
+            Node secondLast=head;
+            Node lastNode=head.next;
+            while(lastNode.next!=null){
+                secondLast=secondLast.next;
+                lastNode=lastNode.next;
+            }
+            secondLast.next=null;
+        }
+    }
+    public static void main(String[] args) {
+        LinkedList list=new LinkedList();
+        list.addFirst("Niranjan");
+        list.addFirst("Vaduni");
+        list.addLast("Here");
+        list.printList();
+        list.deleteFirst();
+        list.printList();
+        list.deleteLast();
+        list.printList();
+    }
+}
