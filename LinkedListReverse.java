c15bc183cf00f3672466b489822d872041ce66e8
Vaduni Niranjan <vaduniniranjan@gmail.com>
2025-01-06 21:36:13 +0530

    Reverse using iterative & Recursive approach

---
+++ LinkedListReverse.java
@@ -0,0 +1,90 @@
+package Java_Code;
+
+public class LinkedListReverse {
+    Node head;
+
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node currentNode = head;
+            while (currentNode.next != null) {
+                currentNode = currentNode.next;
+            }
+            currentNode.next = newNode;
+        }
+    }
+
+    public void printList() {
+        if (head == null) {
+            System.out.println("List is empty");
+        }
+        Node currentNode = head;
+        while (currentNode != null) {
+            System.out.print(currentNode.data + "->");
+            currentNode = currentNode.next;
+        }
+        System.out.println("null");
+    }
+
+    public void reverseIterate() {
+        if (head == null || head.next == null) {
+            return;
+        }
+        Node prevNode = head;
+        Node currNode = head.next;
+        while (currNode != null) {
+            Node nextNode = currNode.next;
+            currNode.next = prevNode;
+            // updating prevNode and currNode
+            prevNode = currNode;
+            currNode = nextNode;
+        }
+        head.next = null;
+        head = prevNode;
+    }
+
+    public Node reverseRecursive(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        Node newHead = reverseRecursive(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+
+    public static void main(String[] args) {
+        LinkedListReverse list = new LinkedListReverse();
+        list.addFirst(2);
+        list.addFirst(1);
+        list.addLast(3);
+        list.addLast(4);
+        list.printList();
+        // list.reverseIterate();
+        // list.printList();
+        list.head = list.reverseRecursive(list.head);
+        list.printList();
+    }
+}
