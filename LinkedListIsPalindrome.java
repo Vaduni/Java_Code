40bb1eab12e8e5f1c7193fbf4f659561c1f54647
Vaduni Niranjan <vaduniniranjan@gmail.com>
2025-01-12 19:09:21 +0530

    Add condition to handle two nodes in list

--- LinkedListIsPalindrome.java
+++ LinkedListIsPalindrome.java
@@ -15,7 +15,7 @@ public class LinkedListIsPalindrome {
 
     // using hare turtle approach
     public LinkedNode findMiddle(LinkedNode head) {
-    LinkedNode hare = head;
+        LinkedNode hare = head;
         LinkedNode turtle = head;
         while (hare.next != null && hare.next.next != null) {
             hare = hare.next.next;
@@ -40,6 +40,12 @@ public class LinkedListIsPalindrome {
         if (head == null || head.next == null) {
             return true;
         }
+        if(head.next.next ==null){
+            if(head.data == head.next.data){
+                return true;
+            }
+            return false;
+        }
         LinkedNode middle = findMiddle(head);
         LinkedNode secondHalfStart = reverse(middle.next);
         LinkedNode firstHalfStart = head;
@@ -52,13 +58,12 @@ public class LinkedListIsPalindrome {
         }
         return true;
     }
+
     public static void main(String[] args) {
-        LinkedListIsPalindrome list=new LinkedListIsPalindrome();
-        LinkedNode head= list.new LinkedNode(1);
-        head.next=list.new LinkedNode(2);
-        head.next.next=list.new LinkedNode(3);
-        head.next.next.next=list.new LinkedNode(4);
-        head.next.next.next.next=list.new LinkedNode(1);
+        LinkedListIsPalindrome list = new LinkedListIsPalindrome();
+        LinkedNode head = list.new LinkedNode(1);
+        head.next = list.new LinkedNode(2);
+        
         System.out.println(list.isPalindrome(head));
     }
 }
