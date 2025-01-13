2548a6c4d81cf4018121265e3ed094c02740623b
Vaduni Niranjan <vaduniniranjan@gmail.com>
2025-01-13 20:58:38 +0530

    Stack Using collection framework

---
+++ StackCollection.java
@@ -0,0 +1,17 @@
+package Java_Code;
+
+import java.util.*;
+
+public class StackCollection {
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
