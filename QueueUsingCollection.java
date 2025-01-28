d95fb6b0cc7b61426630c2c8362161dbbe64601b
Vaduni Niranjan <vaduniniranjan@gmail.com>
2025-01-28 09:59:52 +0530

    Queue using collection framework

---
+++ QueueUsingCollection.java
@@ -0,0 +1,18 @@
+package Java_Code;
+
+import java.util.*;
+import java.util.LinkedList;
+
+public class QueueUsingCollection {
+    public static void main(String[] args) {
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        while (!q.isEmpty()) {
+            System.out.println(q.poll());
+        }
+    }
+
+}
