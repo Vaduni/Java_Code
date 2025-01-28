e847c727ccce6c1d2c2dab86a1fa62c2239cf8ab
Vaduni Niranjan <vaduniniranjan@gmail.com>
2025-01-28 10:00:50 +0530

    Queue using collection framew ork

--- QueueUsingCollection.java
+++ QueueUsingCollection.java
@@ -5,6 +5,7 @@ import java.util.LinkedList;
 
 public class QueueUsingCollection {
     public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
         q.add(1);
         q.add(2);
         q.add(3);
