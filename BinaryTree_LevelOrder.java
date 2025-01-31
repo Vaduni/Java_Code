608aa990454a6bac5652bfded121192431c6ccd5
Vaduni Niranjan <vaduniniranjan@gmail.com>
2025-01-31 10:34:35 +0530

    Traversal using levelOrder

---
+++ BinaryTree_LevelOrder.java
@@ -0,0 +1,68 @@
+//time complexity:O(n)
+package Java_Code;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BinaryTree_LevelOrder {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree {
+        static int idx = -1;
+
+        public static Node buildTree(int nodes[]) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+            return newNode;
+        }
+    }
+
+    public static void LevelOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+        while (!q.isEmpty()) {
+            Node currentNode = q.remove();
+            if (currentNode == null) {
+                System.out.println();
+            }
+            if (currentNode == null) {
+                if (!q.isEmpty()) {
+                    q.add(null);
+                }
+                continue;
+            }
+            System.out.print(currentNode.data + " ");
+            if (currentNode.left != null) {
+                q.add(currentNode.left);
+            }
+            if (currentNode.right != null) {
+                q.add(currentNode.right);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        Node root = BinaryTree.buildTree(nodes);
+        LevelOrder(root);
+    }
+}
