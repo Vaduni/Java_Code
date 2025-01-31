9ca4f3b7efba1aee015ce8e0ed5afc90b68aebe9
Vaduni Niranjan <vaduniniranjan@gmail.com>
2025-01-31 21:54:15 +0530

    Counting total no. of nodes in tree

---
+++ BinaryTree_CountNodes.java
@@ -0,0 +1,47 @@
+//time complexity: O(n)
+
+package Java_Code;
+
+public class BinaryTree_CountNodes {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree {
+        static int idx = -1;
+
+        public static Node buildTree(int nodes[]) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+            return newNode;
+        }
+    }
+
+    public static int CountNodes(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftNodes = CountNodes(root.left);
+        int rightNodes = CountNodes(root.right);
+        return leftNodes + rightNodes + 1;
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        Node root = BinaryTree.buildTree(nodes);
+        System.out.println("Total no. of nodes are "+ CountNodes(root));
+    }
+}
