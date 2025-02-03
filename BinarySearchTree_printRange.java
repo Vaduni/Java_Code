cf2b93bb8410772ec6ee9e9beb5c71d9ece73c52
Vaduni Niranjan <vaduniniranjan@gmail.com>
2025-02-03 11:59:10 +0530

    Print in range

---
+++ BinarySearchTree_printRange.java
@@ -0,0 +1,68 @@
+package Java_Code;
+
+public class BinarySearchTree_printRange {
+    public static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data);
+        inorder(root.right);
+    }
+
+    public static Node insertNode(Node root, int value) {
+        if (root == null) {
+            root = new Node(value);
+            return root;
+        }
+        // left subtree
+        if (root.data > value) {
+            root.left = insertNode(root.left, value);
+        }
+        // right subtree
+        else {
+            root.right = insertNode(root.right, value);
+        }
+        return root;
+    }
+
+    public static Node printRange(Node root, int X, int Y) {
+        if (root == null) {
+            return null;
+        }
+        if (root.data >= X && root.data <= Y) {
+            printRange(root.left, X, Y);
+            System.out.print(root.data);
+            printRange(root.right, X, Y);
+        } else if (root.data <= X) {
+            printRange(root.right, X, Y);
+        } else {
+            printRange(root.left, X, Y);
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+        int Nodes[] = { 5, 1, 3, 4, 2, 7 };
+        Node root = null;
+        for (int i = 0; i < Nodes.length; i++) {
+            root = insertNode(root, Nodes[i]);
+        }
+        inorder(root);
+        System.out.println();
+        printRange(root, 3, 6);
+
+    }
+}
