41e020d87b5c365534c551ab95cde433d9613d84
Vaduni Niranjan <vaduniniranjan@gmail.com>
2025-02-13 20:34:26 +0530

    DFS traversal for deeper traversal

---
+++ Graph_DFS_traversal.java
@@ -0,0 +1,66 @@
+package Java_Code;
+
+import java.util.ArrayList;
+
+public class Graph_DFS_traversal {
+    static class Edge {
+        int src;
+        int dest;
+
+        public Edge(int s, int d) {
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
+    public static void createGraph(ArrayList<Edge> graph[]) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<Edge>();
+        }
+        graph[0].add(new Edge(0, 1));
+        graph[0].add(new Edge(0, 2));
+        graph[1].add(new Edge(1, 0));
+        graph[1].add(new Edge(1, 3));
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 4));
+        graph[3].add(new Edge(3, 1));
+        graph[3].add(new Edge(3, 4));
+        graph[3].add(new Edge(3, 5));
+        graph[4].add(new Edge(4, 2));
+        graph[4].add(new Edge(4, 3));
+        graph[4].add(new Edge(4, 5));
+        graph[5].add(new Edge(5, 3));
+        graph[5].add(new Edge(5, 4));
+        graph[5].add(new Edge(5, 6));
+        graph[5].add(new Edge(6, 5));
+
+    }
+
+    public static void dfs(ArrayList<Edge> graph[], int curr, boolean visited[]) {
+        System.out.print(curr + " ");
+        visited[curr] = true;
+        for (int i = 0; i < graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
+            if (visited[e.dest] == false) {
+                dfs(graph, e.dest, visited);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int v = 7;
+        @SuppressWarnings("unchecked")
+        ArrayList<Edge> graph[] = new ArrayList[v];
+        createGraph(graph);
+        boolean visited[] = new boolean[v];
+        for (int i = 0; i < v; i++) {
+            if (visited[i] == false) {
+                dfs(graph, 0, visited);
+
+            }
+        }
+        System.out.println();
+
+    }
+
+}
